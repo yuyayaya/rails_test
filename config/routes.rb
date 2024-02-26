d53e7ccc9315570f9a1b9702ad190af8f29c3913
kimura-328 <kimkhzxax@outlook.jp>
2024-02-26 14:27:49 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get  "static_pages/about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "application#hello"
   # Defines the root path route ("/")
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -10,4 +11,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_help_url
     assert_response :success
   end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
 end
+
