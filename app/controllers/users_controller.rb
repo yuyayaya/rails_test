7226c86184585957936b1eeac17f2cbaaf7510b0
kimura-328 <kimkhzxax@outlook.jp>
2024-03-04 08:18:57 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -24,5 +25,5 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-  
 end
+
